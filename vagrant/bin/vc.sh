eb221154da819f5205df43ac423059f2625d4429
pmelab <philipp.melab@gmail.com>
2014-03-19 09:20:36 +0100

    Fix in command sh, php performance tuning.

--- vagrant/bin/vc.sh
+++ vagrant/bin/vc.sh
@@ -6,7 +6,7 @@ args=${@:3:$length}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $(pgrep -f "vagrant@arroyo.local") > 0 ]; then
-  ssh -i ~/.vagrant.d/insecure_private_key vagrant@arroyo.local -F $DIR/../vssh-config "cd $cwd; $command $args";
+  ssh -i ~/.vagrant.d/insecure_private_key vagrant@arroyo.local -F $DIR/../vssh-config " cd $cwd; $command $args";
 else
   ssh -MNf -i ~/.vagrant.d/insecure_private_key vagrant@arroyo.local -F $DIR/../vssh-config && ssh -i ~/.vagrant.d/insecure_private_key vagrant@arroyo.local -F $DIR/../vssh-config"cd $cwd; $command $args";
 fi
