e78344195446d661efb64c7bef9e9329feb0eba5
pmelab <philipp.melab@gmail.com>
2014-03-24 16:11:10 +0100

    added ".local" suffix to local aliases

--- vagrant/puppet/custom/modules/arroyo/files/aliases.drushrc.php
+++ vagrant/puppet/custom/modules/arroyo/files/aliases.drushrc.php
@@ -2,7 +2,7 @@
 // Standard aliases for everything thats a drupal directory.
 foreach (glob('/var/www/*/profiles') as $file) {
   $platform = preg_replace('/^\/var\/www\/([a-z]+)\/profiles$/', '$1', $file);
-  $aliases[$platform] = array(
+  $aliases[$platform . ".local"] = array(
     'uri' => 'http://arroyo.local/drupal/' . $platform,
     'root' => '/var/www/' . $platform,
     'db-url' => 'mysql://root:root@localhost:3306/' . $platform,
